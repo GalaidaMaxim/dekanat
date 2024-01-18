c7487cebeb44577c290776d0c5af9a549fb4f3eb
MaximKucheriaviy <96634366+MaximKucheriaviy@users.noreply.github.com>
2024-01-19 00:44:28 +0200

    fix

--- electron/DBActions/addMandatorySubjects.js
+++ electron/DBActions/addMandatorySubjects.js
@@ -3,7 +3,6 @@ const { Subjects, Departments, Students } = require("../models");
 module.exports = async (studentId) => {
   const student = await Students.findById(studentId).populate("department");
   const madatorySubjects = await Subjects.find({
-    department: student.department._id,
     mandatory: true,
     level: student.level,
   });
