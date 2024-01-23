8dc5f26133790544cafd66729d3ae9e4e8b67583
Maxim Kucheriaviy <maxim-k@i.ua>
2024-01-23 14:08:34 +0200

    fixes

--- electron/exelTables/replaseBracers.js
+++ electron/exelTables/replaseBracers.js
@@ -2,8 +2,8 @@ module.exports = (worksheet, searchText, newText) => {
   worksheet.eachRow({ includeEmpty: false }, (row, rowNumber) => {
     row.eachCell({ includeEmpty: false }, (cell, colNumber) => {
       if (
-        typeof cell.value === "object" &&
         cell.value !== null &&
+        typeof cell.value === "object" &&
         cell.value.richText.some((item) => item.text.includes(searchText))
       ) {
         const index = cell.value.richText.findIndex((item) =>
@@ -12,10 +12,12 @@ module.exports = (worksheet, searchText, newText) => {
         cell.value.richText[index].text = cell.value.richText[
           index
         ].text.replace(searchText, newText);
-      } else {
-        if (cell.value === searchText) {
-          cell.value = newText;
-        }
+      } else if (
+        cell.value !== null &&
+        typeof cell.value === "string" &&
+        cell.value.includes(searchText)
+      ) {
+        cell.value = cell.value.replace(searchText, newText);
       }
     });
   });
--- electron/exelTables/vidomist.js
+++ electron/exelTables/vidomist.js
@@ -4,18 +4,52 @@ const path = require("path");
 const checkOutputDerectory = require("./checkOutputDerectory");
 const filePath = path.join(require("os").homedir(), "Деканат Файли");
 const replaseBracers = require("./replaseBracers");
-const main = async (fileName) => {
+const { log } = require("console");
+
+const main = async (
+  fileName,
+  options = {
+    DEPARTMENT: "",
+    OS: "",
+    PROFILE: "",
+    COURSE: "I",
+    BEGIN_YEAR: "2021",
+    END_YEAR: "2022",
+    SUBJECT: "",
+    SEMSTER: "",
+    HOURS: "",
+    COACH: "",
+    COACH2: "",
+  }
+) => {
   try {
     const workBook = new ExcelJS.Workbook();
+    options.DEPARTMENT = options.DEPARTMENT.toLocaleUpperCase();
+    options.PROFILE = options.PROFILE.toLocaleUpperCase();
+    options.OS = options.OS.toLocaleUpperCase();
     await workBook.xlsx.readFile(
       path.resolve(__dirname, "templates", "TempColedgVidom.xlsx")
     );
-    replaseBracers(workBook.getWorksheet(1), "{OS}", "Магістр");
-    console.log(workBook.getWorksheet(1).getCell("A4").set);
+    Object.keys(options).forEach((item) => {
+      replaseBracers(workBook.getWorksheet(1), `{${item}}`, options[item]);
+    });
+    console.log(workBook.getWorksheet(1).getCell("A43").value);
     checkOutputDerectory(filePath);
     workBook.xlsx.writeFile(path.resolve(filePath, fileName));
   } catch (err) {
     console.log(err);
   }
 };
-main("Vido.xlsx");
+main("Vido.xlsx", {
+  DEPARTMENT: "Коледж",
+  OS: "Молодший бакалавр",
+  PROFILE: "ФЛЕЙТА",
+  COURSE: "II",
+  BEGIN_YEAR: "2024",
+  END_YEAR: "2025",
+  SEMSTER: 3,
+  SUBJECT: "Оркестр",
+  CONTROL_TYPE: "екзамен",
+  COACH: "Бодіна В. В.",
+  HOURS: 16,
+});
