a6af7d4f8961dfc33a243874cac1ef8afdcbd4ef
Maxim Kucheriaviy <maxim-k@i.ua>
2025-07-02 14:30:12 +0300

    under 60 fix

--- aplication/src/pages/DebitReport.jsx
+++ aplication/src/pages/DebitReport.jsx
@@ -47,7 +47,8 @@ export const DebitReport = () => {
         const subjects = student.subjects.filter(
           (subject) =>
             subject.semesters[semester - 1].include &&
-            !subject.semesters[semester - 1].mark &&
+            (!subject.semesters[semester - 1].mark ||
+              subject.semesters[semester - 1].mark < 60) &&
             !subject.semesters[semester - 1].ignore
         );
         subjects.forEach((subject) => {
