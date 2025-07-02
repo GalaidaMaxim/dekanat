9276e6459b889128b301a65a4808cef1a5db5ca2
Maxim Kucheriaviy <maxim-k@i.ua>
2025-07-02 15:27:45 +0300

    1.1.0

--- aplication/src/componetns/LaunchWindow.jsx
+++ aplication/src/componetns/LaunchWindow.jsx
@@ -12,7 +12,7 @@ export const LaounchWindow = ({ setType }) => {
   const dbConnected = useDBConnected();
   const updated = useUpdated();
   const dispatch = useDispatch();
-  const version = "1.0.1";
+  const version = "1.1.0";
   useEffect(() => {
     if (!dbConnected) {
       return;
