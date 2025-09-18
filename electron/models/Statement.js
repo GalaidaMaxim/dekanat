fe75baa0aa5b8f809586a33692e237e68e11ae37
Maxim Kucheriaviy <maxim-k@i.ua>
2025-09-18 13:11:57 +0300

    startments modelt ready

---
+++ electron/models/Statement.js
@@ -0,0 +1,65 @@
+const { Schema, model } = require("mongoose");
+
+const StatmentSchema = new Schema({
+  department: {
+    type: Schema.Types.ObjectId,
+    ref: "department",
+    require: true,
+  },
+  educationPlan: {
+    type: Schema.Types.ObjectId,
+    ref: "educationPlan",
+  },
+  course: {
+    type: Number,
+    trim: true,
+    require: true,
+  },
+  semester: {
+    type: Number,
+    trim: true,
+    require: true,
+  },
+  subject: {
+    type: Schema.Types.ObjectId,
+    ref: "subject",
+    require: true,
+  },
+  facultet: {
+    type: Schema.Types.ObjectId,
+    ref: "facultet",
+    require: true,
+  },
+  remoteType: {
+    type: String,
+    trim: true,
+    require: true,
+  },
+  code: {
+    type: String,
+    trim: true,
+    require: true,
+  },
+  year: {
+    type: String,
+    trim: true,
+    require: true,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+    require: true,
+  },
+  decan: {
+    type: String,
+    trim: true,
+    require: true,
+  },
+  group: {
+    type: String,
+    default: "А",
+    enum: ["А", "Б", "В", "Г", "Д"],
+  },
+});
+
+module.exports = model("statment", StatmentSchema);
